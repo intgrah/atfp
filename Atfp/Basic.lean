aa93af959cad03ca8b2335fbda3cafdf716bc450
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-02-22 07:38:32 +0000

    Semiring stuff

--- Atfp/Basic.lean
+++ Atfp/Basic.lean
@@ -1,6 +1,11 @@
 import Mathlib.Algebra.Category.Grp.Basic
 import Mathlib.Algebra.Category.MonCat.Basic
 import Mathlib.Algebra.Category.MonCat.Limits
+import Mathlib.Algebra.Module.Basic
+import Mathlib.Algebra.Order.Kleene
+import Mathlib.Algebra.Order.Ring.Basic
+import Mathlib.Algebra.Ring.Basic
+import Mathlib.Algebra.Tropical.Basic
 import Mathlib.CategoryTheory.Category.Basic
 import Mathlib.CategoryTheory.Category.Pointed
 import Mathlib.CategoryTheory.Category.RelCat
@@ -17,7 +22,12 @@ import Mathlib.CategoryTheory.Monoidal.Closed.Basic
 import Mathlib.CategoryTheory.Monoidal.FunctorCategory
 import Mathlib.CategoryTheory.Monoidal.Types.Basic
 import Mathlib.CategoryTheory.Types.Basic
+import Mathlib.Combinatorics.Digraph.Basic
 import Mathlib.Computability.ContextFreeGrammar
+import Mathlib.Computability.Language
+import Mathlib.Data.ENat.Basic
+import Mathlib.Topology.UnitInterval
+import Mathlib.Data.Set.Semiring
 import Mathlib.Data.Matrix.Basic
 import Mathlib.Data.Sum.Order
 import Mathlib.GroupTheory.Coprod.Basic
@@ -1241,7 +1251,7 @@ notation "[" e "]ᵈ" => Tm.disc e
 notation "[" Γ "]ᵈ" => Ctx.disc Γ
 
 set_option hygiene false in
-notation:max Γ " ⊢ " e " : " A => HasType Γ e A
+scoped notation:max Γ " ⊢ " e " : " A => HasType Γ e A
 
 inductive HasType : Ctx → Tm → Ty → Type u
   | var {Γ} x A :
@@ -1993,7 +2003,331 @@ end Chapter4
 
 section Chapter5
 
+universe u
+
+variable {N : Type u} (G : Digraph N)
+
+#check G.Adj
+
+def exampleDigraph : Digraph (Fin 4) where
+  Adj
+   | 0, 0
+   | 0, 1
+   | 1, 2
+   | 3, 0 => True
+   | _, _ => False
+
+def Digraph.adjMatrix (G : Digraph N) [DecidableRel G.Adj] : Matrix N N Bool :=
+  (G.Adj · ·)
+
+-- #eval exampleDigraph.adjMatrix
+
+section Section1
+
+/-! Definition 5.1.1 -/
+
+variable {S : Type u} [inst : Semiring S]
+#check Semiring
+#check inst.zero
+#check inst.one
+#check inst.add
+#check inst.mul
+#check inst.toAddCommMonoid
+#check inst.toMonoid.mul
+#check inst.right_distrib
+#check inst.left_distrib
+
+/-! Example 5.1.2 -/
+
+instance : AddZero Bool where
+  zero := false
+  add := or
+
+instance : AddCommMonoid Bool where
+  zero_add := Bool.false_or
+  add_zero := Bool.or_false
+  add_assoc := Bool.or_assoc
+  add_comm := Bool.or_comm
+  nsmul := nsmulRec
+
+instance : Monoid Bool where
+  one := ⊤
+  mul := and
+  one_mul := Bool.true_and
+  mul_one := Bool.and_true
+  mul_assoc := Bool.and_assoc
+
+instance : Semiring Bool where
+  zero_mul := Bool.false_and
+  mul_zero := Bool.and_false
+  left_distrib := Bool.and_or_distrib_left
+  right_distrib := Bool.and_or_distrib_right
+
+/-! Example 5.1.3 -/
+
+#synth Semiring Nat
+
+/-! Example 5.1.4 -/
+
+#synth Semiring Int
+
+/-! Example 5.1.5 -/
+
+variable {M : Type u} [Monoid M]
+#synth Semiring (SetSemiring M)
+
+/-! Example 5.1.6 -/
+
+#synth Semiring (Tropical ℕ∞)
+
+section Section1
+
+/-! Definition 5.1.7 -/
+
+variable {S : Type u} [inst : CommSemiring S]
+#check CommSemiring
+#check inst.mul_comm
+
+/-! Definition 5.1.8 -/
+
+variable {S : Type u} [Semiring S] [PartialOrder S] [IsOrderedRing S]
+#check IsOrderedRing
+#check add_le_add (α := S)
+#check mul_le_mul (α := S)
+
+/-! Definition 5.1.9 -/
+
+open scoped Computability in
+class ClosedSemiring (α : Type u) extends Semiring α, PartialOrder α, KStar α where
+  kstar_eq_one_add_mul_kstar : ∀ a : α, a∗ = 1 + a * a∗
+  kstar_eq_one_add_kstar_mul : ∀ a : α, a∗ = 1 + a∗ * a
+  kstar_induction_left : ∀ a b x : α, b + a * x ≤ x → a∗ * b ≤ x
+  kstar_induction_right : ∀ a b x : α, b + x * a ≤ x → b * a∗ ≤ x
+
+/-! Example 5.1.10 -/
+
+-- TODO monoid powerset
+
+/-! Example 5.1.11 -/
+
+instance : IsOrderedRing Bool where
+  add_le_add_left a b hab c := by
+    change a || c → b || c
+    intro g
+    rw [Bool.or_eq_true] at g ⊢
+    cases g with
+    | inl ha => exact .inl (hab ha)
+    | inr hc => exact .inr hc
+  zero_le_one := Bool.false_lt_true.le
+  mul_le_mul_of_nonneg_left a ha b c hbc hab := by
+    change a && c
+    rw [Bool.and_eq_true]
+    change a && b at hab
+    rw [Bool.and_eq_true] at hab
+    exact ⟨hab.left, hbc hab.right⟩
+  mul_le_mul_of_nonneg_right a ha b c hbc hab := by
+    change c && a
+    rw [Bool.and_eq_true]
+    change b && a at hab
+    rw [Bool.and_eq_true] at hab
+    exact ⟨hbc hab.left, hab.right⟩
+
+instance : ClosedSemiring Bool where
+  kstar _ := ⊤
+  kstar_eq_one_add_mul_kstar _ := rfl
+  kstar_eq_one_add_kstar_mul _ := rfl
+  kstar_induction_left a b x h hb := by
+    change b || a * x → x at h
+    rw [Bool.or_eq_true] at h
+    exact h (.inl hb)
+  kstar_induction_right a b x h hb := by
+    change b && true at hb
+    rw [Bool.and_true] at hb
+    change b || x * a → x at h
+    rw [Bool.or_eq_true] at h
+    exact h (.inl hb)
+
+-- TODO
+
+/-! Example 5.1.12 -/
+
+instance : ClosedSemiring (Tropical ℕ∞) := sorry
+
+/-! Example 5.1.13 -/
+
+instance : ClosedSemiring ℕ∞ where
+  kstar
+    | 0 => 1
+    | _ => ⊤
+  kstar_eq_one_add_mul_kstar
+    | ⊤ => rfl
+    | 0 => rfl
+    | some (n + 1) => rfl
+  kstar_eq_one_add_kstar_mul
+    | ⊤ => rfl
+    | 0 => rfl
+    | some (n + 1) => rfl
+  kstar_induction_left a b x h := by
+    cases a using ENat.nat_induction <;>
+    cases b using ENat.nat_induction <;>
+    cases x using ENat.nat_induction
+    case succ.succ.succ =>
+      exfalso
+      norm_cast at h
+      simp only [Nat.succ_mul, Nat.mul_succ] at h
+      omega
+    all_goals simp_all
+  kstar_induction_right a b x h := by
+    cases a using ENat.nat_induction <;>
+    cases b using ENat.nat_induction <;>
+    cases x using ENat.nat_induction
+    case succ.succ.succ =>
+      exfalso
+      norm_cast at h
+      simp only [Nat.succ_mul, Nat.mul_succ] at h
+      omega
+    all_goals simp_all
+
+/-! Definition 5.1.14 -/
+
+variable {S : Type u} [inst : IdemSemiring S]
+#check IdemSemiring
+#check add_idem
+
+/-! Definition 5.1.15 -/
+
+#check inst.toPartialOrder
+#check inst.toSemilatticeSup
+#check inst.bot_le
+
+/-! Lemma 5.1.16 -/
+
+#check inst.toMulLeftMono
+#check inst.toMulRightMono
+
+/-! Definition 5.1.17 -/
+
+variable {S : Type u} [inst : KleeneAlgebra S]
+#check KleeneAlgebra
+#check inst.toIdemSemiring
+#check inst.kstar
+-- TODO
+
+/-! Non-Example 5.1.18 -/
+
+example : ¬∃ ka : KleeneAlgebra ℕ∞, ka.toSemiring = instCommSemiringENat.toSemiring := by
+  intro ⟨kleene, ha⟩
+  have : (3 : ℕ∞) + 3 = 6 := rfl
+  have := add_idem (3 : ℕ∞)
+  simp_all
+
+/-! Example 5.1.19 -/
+
+variable {α : Type u}
+#synth KleeneAlgebra (Language α)
+
+/-! Lemma 5.1.20 -/
+
+#check Language.self_eq_mul_add_iff
+
+end Section1
+
+end Section1
+
+section Section2
+
+/-! Definition 5.2.1 -/
+
+#check Module
+-- TODO
+
+end Section2
+
+section Section3
+
+-- TODO
+
+end Section3
+
+section Section4
+
+-- TODO
+
+end Section4
+
+section Section5
+
+def graph : Digraph (Fin 2) where
+  Adj _ _ := True
+
+section Section1
+
+variable {Q : Type u} {G : Digraph Q} [DecidableRel G.Adj]
+
+def reachability : Digraph Q := sorry
+
+end Section1
+
+section Section2
+
+-- TODO path counting
+
+end Section2
+
+section Section3
+
+-- TODO APSP
+
+end Section3
+
+section Section4
+
+-- TODO Kleene's algorithm
+
+end Section4
+
+section Section5
+
+open scoped unitInterval
+
+instance : Add I where
+  add a b := ⟨max a.1 b.1, by aesop⟩
+
+instance : AddCommMonoid I where
+  zero_add a := max_eq_right zero_le'
+  add_zero a := max_eq_left zero_le'
+  add_assoc a b c := Subtype.ext (max_assoc a.1 b.1 c.1)
+  add_comm a b := Subtype.ext (max_comm a.1 b.1)
+  nsmul := nsmulRec
+
+instance : Semiring I where
+  left_distrib a b c := Subtype.ext (mul_max_of_nonneg b.1 c.1 (Set.mem_Icc.mp a.2).1)
+  right_distrib a b c := Subtype.ext (max_mul_of_nonneg a.1 b.1 (Set.mem_Icc.mp c.2).1)
+
+instance : ClosedSemiring I where
+  kstar p := 1
+  kstar_eq_one_add_mul_kstar a :=
+    max_top_left _ |>.symm
+  kstar_eq_one_add_kstar_mul a :=
+    max_top_left _ |>.symm
+  kstar_induction_left a b x h := by
+    calc 1 * b
+        = b := one_mul b
+      _ ≤ max b (a * x) := le_max_left b (a * x)
+      _ ≤ x := h
+  kstar_induction_right a b x h := by
+    calc b * 1
+        = b := mul_one b
+      _ ≤ max b (x * a) := le_max_left b (x * a)
+      _ ≤ x := h
+
+-- TODO Viterbi
+
+end Section5
+
 -- TODO graph algorithms
+
+end Section5
 
 end Chapter5
 
