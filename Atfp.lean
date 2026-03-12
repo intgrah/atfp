c553c0f32729d1fb17799769976106d8a26d0c4a
IntGrah <148660026+IntGrah@users.noreply.github.com>
2026-03-12 21:56:02 +0000

    more

--- Atfp.lean
+++ Atfp.lean
@@ -1,5 +1,7 @@
-import Atfp.Chapter2
-import Atfp.Chapter3
-import Atfp.Chapter4
-import Atfp.Chapter5
-import Atfp.Chapter6
+module
+
+public import Atfp.Chapter2
+public import Atfp.Chapter3
+public import Atfp.Chapter4
+public import Atfp.Chapter5
+public import Atfp.Chapter6
